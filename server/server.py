5f01a4fa7f01631df8416e7f4ab57db2c9bd969a
SledDev <90392654+sleddev@users.noreply.github.com>
2023-05-29 15:09:57 +0200

    register, login, refresh

--- server/server.py
+++ server/server.py
@@ -2,12 +2,13 @@ import base64
 import math
 import secrets
 import hashlib
-import random
+import re
 
 from typing import Literal
 from datetime import datetime
 
 from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import dotenv_values
@@ -23,10 +24,13 @@ mongo_uri = env["MONGO_URI"]
 mongo = MongoClient(mongo_uri, server_api=ServerApi('1'))
 db = mongo[env["DATABASE_NAME"]]
 
+access_token_expiry = 3_600_000 # one hour
+verify_email_expiry = 1_200_000 # 20 minutes
+
 ##### Middleware #####
 
 open_endpoints = [
-  '/', '/auth/login', '/auth/register', 'auth/refresh',
+  '/', '/auth/login', '/auth/register', '/auth/refresh',
   '/auth/isavailable', '/auth/verify_email', '/auth/verify_email/send',
   '/openapi.json', '/docs', '/redoc'
 ]
@@ -73,18 +77,24 @@ async def is_available(response: Response, username: str = None, email: str = No
   response.status_code = 400
   return {"message": "Provide either email or username"}
 
+class VerifyEmailBody(BaseModel):
+  username: str = ""
+  code: str = ""
 @app.post('/auth/verify_email')
-async def verify_email(response: Response, user: str = "", code: str = ""):
+async def verify_email(response: Response, body: VerifyEmailBody = None):
   timestamp: int = math.floor(datetime.now().timestamp() * 1000)
-  doc = db['email-verification'].find_one(find_email_verification(user), {"_id": 0})
+  if body is None:
+    response.status_code = 400
+    return {"error": "Invalid request body"}
+  doc = db['email-verification'].find_one(find_email_verification(body.username), {"_id": 0})
   email, digits = (doc["email"], str(doc["code"])) if doc is not None else ("asd", "fgh")
   expected = await get_email_code(email, digits)
   print(expected)
-  if not secrets.compare_digest(expected, bytes(code, "utf-8")) or doc is None or timestamp >= doc["expires_at"]:
+  if not secrets.compare_digest(expected, bytes(body.code, "utf-8")) or doc is None or timestamp >= doc["expires_at"]:
     response.status_code = 400
     if doc is not None:
       db['email-verification'].update_one(
-        find_email_verification(user),
+        find_email_verification(body.username),
         {
           "$inc": {"attempts": 1}
         }
@@ -95,21 +105,27 @@ async def verify_email(response: Response, user: str = "", code: str = ""):
     return {"error": "Invalid username or code"}
   
   db['users'].update_one(
-    find_user_from_name(user),
+    find_user_from_name(body.username),
     { "$set": {"verified": True} }
   )
-  db['email-verification'].delete_one(find_user_from_name(user))
+  db['email-verification'].delete_one(find_user_from_name(body.username))
   return {"message": "Verified successfully!"}
   
+class SendEmailBody(BaseModel):
+  username: str = ""
+  email: str = ""
 @app.post('/auth/verify_email/send')
-async def send_verification_email(response: Response, username: str = "", email: str = ""):
+async def send_verification_email(response: Response, body: SendEmailBody = None):
   timestamp: int = math.floor(datetime.now().timestamp() * 1000)
-  user = db['users'].find_one(find_user_from_name(username))
-  if user is None or user["email"] != email.lower() or user["verified"]:
+  if body is None:
+    response.status_code = 400
+    return {"error": "Invalid request body"}
+  user = db['users'].find_one(find_user_from_name(body.username))
+  if user is None or user["email"] != body.email.lower() or user["verified"]:
     response.status_code = 400
     return {"error": "Invalid username or email, or already verified. Wait one minute between sending emails."}
   
-  doc = db['email-verification'].find_one(find_email_verification(username), {"_id": 0})
+  doc = db['email-verification'].find_one(find_email_verification(body.username), {"_id": 0})
   if doc is None:
     db['email-verification'].insert_one({
       "username": user["username"],
@@ -117,36 +133,144 @@ async def send_verification_email(response: Response, username: str = "", email:
       "attempts": 0,
       "code": secrets.randbelow(900_000) + 100_000,
       "created_at": timestamp,
-      "expires_at": timestamp + 1_200_000
+      "expires_at": timestamp + verify_email_expiry
     })
   else:
     if doc["created_at"] > timestamp - 60_000:
       response.status_code = 400
       return {"error": "Invalid username or email, or already verified. Wait one minute between sending emails."}
     db['email-verification'].update_one(
-      find_email_verification(username),
+      find_email_verification(body.username),
       {"$set": {
         "attempts": 0,
         "code": secrets.randbelow(900_000) + 100_000,
         "created_at": timestamp,
-        "expires_at": timestamp + 1_200_000
+        "expires_at": timestamp + verify_email_expiry
       }}
     )
-  
   #TODO: send email
   return {"message": "Email sent"}
   
+class RegisterBody(BaseModel):
+  username: str = None
+  display_name: str = None
+  password: str = None
+  email: str = None
 @app.post('/auth/register')
-async def register():
-  pass
+async def register(response: Response, body: RegisterBody = None):
+  timestamp: int = math.floor(datetime.now().timestamp() * 1000)
+  
+  if body is None or body.username is None or body.display_name is None \
+    or body.password is None or body.email is None:
+    response.status_code = 400
+    return {"error": "Invalid request body"}
+  if db['users'].find_one(find_user_from_name(body.username)) is not None:
+    response.status_code = 409
+    return {"error": "Username taken"}
+  if db['users'].find_one(find_user_from_email(body.email)) is not None:
+    response.status_code = 409
+    return {"error": "Email taken"}
+  if re.fullmatch(r"[^@]+@[^@]+\.[^@]+", body.email) is None:
+    response.status_code = 400
+    return {"error": "Invalid email address"}
+  
+  db['users'].insert_one({
+    "username": body.username,
+    "display_name": body.display_name,
+    "password": base64.encodebytes(hashlib.scrypt(
+      bytes(body.password, "utf-8"),
+      salt=bytes(server_secret + body.username, "utf-8"),
+      n=8192, r=24, p=1,dklen=64
+    )).decode("utf-8"),
+    "email": body.email,
+    "verified": False,
+    "preferences": {},
+    "images": {
+      "profile_pic": "",
+      "banner": ""
+    },
+    "created_at": timestamp,
+    "last_login": 0,
+    "refresh_tokens": []
+  })
+  response.status_code = 201
+  return {"message": "Registered successfully"}
 
+class LoginBody(BaseModel):
+  username: str = None
+  password: str = None
 @app.post('/auth/login')
-async def register():
-  pass
+async def login(response: Response, body: LoginBody = None):
+  timestamp: int = math.floor(datetime.now().timestamp() * 1000)
 
+  if body is None or body.username is None or body.password is None:
+    response.status_code = 400
+    return {"error": "Invalid request body"}
+  user = db['users'].find_one(find_user_from_name(body.username))
+  pass_hash = base64.encodebytes(hashlib.scrypt(
+    bytes(body.password, "utf-8"),
+    salt=bytes(server_secret + body.username, "utf-8"),
+    n=8192, r=24, p=1,dklen=64
+  )).decode("utf-8")
+  if user is None or not secrets.compare_digest(pass_hash, user["password"]):
+    response.status_code = 401
+    return {"error": "Invalid username or password"}
+  if not user["verified"]:
+    response.status_code = 401
+    return {"error": "Email verification needed"}
+  
+  refresh_token, refresh_timestamp = await generate_token(body.username, "refresh")
+  access_token, access_timestamp = await generate_token(body.username, "access")
+  db['users'].update_one(
+    find_user_from_name(body.username),
+    {
+      "$set": { "last_login": timestamp },
+      "$push": { "refresh_tokens": {
+        "token": refresh_token,
+        "created_at": refresh_timestamp,
+        "access_tokens": [{
+          "token": access_token,
+          "created_at": access_timestamp,
+          "expires_at": access_timestamp + access_token_expiry
+        }]
+      }}
+    }
+  )
+  return {
+    "refresh_token": refresh_token,
+    "access_token": access_token,
+    "expires_at": access_timestamp + access_token_expiry
+  }
+
+class RefreshBody(BaseModel):
+  refresh_token: str = None
 @app.post('/auth/refresh')
-async def refresh():
-  pass
+async def refresh(response: Response, body: RefreshBody = None):
+  timestamp: int = math.floor(datetime.now().timestamp() * 1000)
+  if body is None or body.refresh_token is None:
+    response.status_code = 400
+    return {"error": "Invalid request body"}
+  user = db['users'].find_one(find_user_from_refresh_token(body.refresh_token))
+  if user is None:
+    response.status_code = 401
+    return {"error": "Invalid refresh token"}
+  access_token, access_timestamp = await generate_token(user["username"])
+  db['users'].update_one(
+    {
+      "username": user["username"],
+      "refresh_tokens.token": body.refresh_token
+    },
+    { "$push": {"refresh_tokens.$.access_tokens": {
+      "token": access_token,
+      "created_at": access_timestamp,
+      "expires_at": access_timestamp + access_token_expiry
+    }} }
+  )
+  return {
+    "token": access_token,
+    "created_at": access_timestamp,
+    "expires_at": access_timestamp + access_token_expiry
+  }
 
 async def get_token_signature(username: str, creation_time: int, token_type: Literal["access", "refresh"] = "access"):
   root_string: str = str(creation_time) + username + server_secret
